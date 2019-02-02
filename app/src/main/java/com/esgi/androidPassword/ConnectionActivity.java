b54b949575aeedd6edffbe13a72b32b9d75d00dd
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-02-02 23:59:58 +0100

    remove unused import

--- app/src/main/java/com/esgi/androidPassword/ConnectionActivity.java
+++ app/src/main/java/com/esgi/androidPassword/ConnectionActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.esgi.androidPassword.R;
-
 public class ConnectionActivity extends AppCompatActivity {
 
     @Override
