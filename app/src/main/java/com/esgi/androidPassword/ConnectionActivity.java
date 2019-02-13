4f8bb6acaeb73eb3dd5ce38449a112fce1828673
Mickael Moreira <dopidot@gmail.com>
2019-02-13 23:30:56 +0100

    - CreationActivity design added (in progress...) - Change static text to strings.xml text - Set ConnectionActivity to default started activity

--- app/src/main/java/com/esgi/androidPassword/ConnectionActivity.java
+++ app/src/main/java/com/esgi/androidPassword/ConnectionActivity.java
@@ -1,5 +1,6 @@
 package com.esgi.androidPassword;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,12 +14,15 @@ public class ConnectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
 
+        final Intent intent = new Intent(this, CreationActivity.class);
+
         Button validateButton = findViewById(R.id.validate);
         validateButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Clicked !", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(v.getContext(), "Clicked !", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/esgi/androidPassword/MainActivity.java
+++ app/src/main/java/com/esgi/androidPassword/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ConnectionActivity.class);
-        startActivity(intent);
+        /*Intent intent = new Intent(this, ConnectionActivity.class);
+        startActivity(intent);*/
     }
 }
